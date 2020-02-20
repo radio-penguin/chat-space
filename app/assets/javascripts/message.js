fe96a9db4b52dc7a43615b1403e2b08f912dae96
radio-penguin <m.atsushi1745@gmail.com>
2020-02-20 17:03:24 +0900

    自動更新実装中

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     if ( message.image ) {
-      var html =
+      var html = `<div class="message" data-message-id=` + message.id + `>`
        `<div class="message-list__box" data-message-id=${message.id}>
           <div class="message-list__box__info">
             <div class="message-list__box__info__talker">
@@ -20,7 +20,7 @@ $(function(){
         </div>`
       return html;
     } else {
-      var html =
+      var html = `<div class="message" data-message-id=` + message.id + `>`
        `<div class="message-list__box" data-message-id=${message.id}>
           <div class="message-list__box__info">
             <div class="message-list__box__info__talker">
@@ -39,6 +39,7 @@ $(function(){
       return html;
     };
   }
+  
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -63,4 +64,30 @@ $(function(){
       alert("メッセージ送信に失敗しました");
   });
   });
+  
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.message-list').append(insertHTML);
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+  }
 });
